266a3b8cd3bc5b7d9f232d36837d305d9916502d
Shardul <shardul.kool31@gmail.com>
2022-03-31 10:20:55 +0530

    commit 4

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from crypt import methods
-from flask import Flask, render_template, request , url_for
+from flask import Flask, redirect, render_template, request , url_for
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ def index():
 
     if request.method == 'POST':
         content=request.form
-        return(content['username'])
+        return redirect('/products')
     else:
         return render_template('index.html')
 
@@ -20,6 +20,24 @@ def register():
         return(content['firstname'])
     else:
         return render_template('register.html')
+
+@app.route('/products', methods=['POST','GET'])
+def products():
+
+    if request.method == 'POST':
+        content=request.form
+        return(content['firstname'])
+    else:
+        return render_template('Products.html')
+@app.route('/cart', methods=['POST','GET'])
+def cart():
+
+    if request.method == 'POST':
+        content=request.form
+        return(content['firstname'])
+    else:
+        return render_template('cart.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
